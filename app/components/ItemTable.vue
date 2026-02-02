3f19594d69c0384f1f900dc200e71d49d69bf1a5
CalvinRostas <cro@3nrg.de>
2026-02-02 17:59:37 +0100

    Add Nuxt + Capacitor Android starter scaffold
    - Initial setup for nuxt + ionic + capacitor
    - also added Tailwind and UI Thing
    - start for app

---
+++ app/components/ItemTable.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p>Gegenstände</p>
+        <div class="w-full">
+            <div class="py-3">
+                <UiDivider class="pb-3" />
+                <div class="w-full flex items-center px-3">
+                    <div class="flex-1">
+                        <UiAvatar src="https://picsum.photos/128" alt="Colm Tuite" fallback="CT" :delay-ms="600" />
+                    </div>
+                    <div class="flex-1">
+                        <p>Colm Tuite</p>
+                    </div>
+                    <div class="flex-1 flex justify-end">
+                        <UiButton size="icon" variant="outline">
+                            <Icon class="size-4" name="lucide:pen" />
+                        </UiButton>
+                    </div>
+                </div>
+                <UiDivider class="pt-3" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
