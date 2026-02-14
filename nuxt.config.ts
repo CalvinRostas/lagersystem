780ab9ec582eed8b4ef4573d94f959a1db887934
Luca Recke <reckeluca.oz@gmail.com>
2026-02-14 22:46:11 +0100

    add temp db

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
     "@vueuse/nuxt",
     "@nuxt/icon",
     "@nuxt/fonts",
+    "@nuxthub/core"
   ],
 
   css: ["./app/assets/css/main.css"],
@@ -51,4 +52,8 @@ export default defineNuxtConfig({
     fetchTimeout: 2000,
     serverBundle: "local",
   },
+
+  hub: {
+    db: 'sqlite'
+  }
 });
